b0a1b1752f7ef25683c333019f2d281208214c90
ibaveda <igorbaveda@gmail.com>
2019-03-20 15:58:11 -0600

    pushing to heroku

---
+++ server.js
@@ -0,0 +1,46 @@
+//dependencies
+var mongoose = require('mongoose');
+var logger = require('morgan');
+
+//initialize Express app
+var express = require('express');
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(process.cwd() + '/public'));
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+//connecting to MongoDB
+
+var databaseUri = 'mongodb://localhost/climbing_news';
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+  mongoose.connect(databaseUri);
+}
+// mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds121026.mlab.com:21026/heroku_k9kc44w1');
+
+
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to Mongoose!')
+});
+
+var routes = require('./controller/controller.js');
+app.use('/', routes);
+
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log('Listening on PORT ' + port);
+});
